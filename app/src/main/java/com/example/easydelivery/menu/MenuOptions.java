abbbb47321e27e61321bb23349cd7c20bee0aed4
Rafael-Guerrero-Moreira <rafael.guerrero2016@uteq.edu.ec>
2021-02-23 09:33:52 -0500

    Menu de opciones

--- app/src/main/java/com/example/easydelivery/menu/MenuOptions.java
+++ app/src/main/java/com/example/easydelivery/menu/MenuOptions.java
@@ -9,19 +9,30 @@ public class MenuOptions {
     private static JSONObject ORDERS;
     private static JSONObject REPORTS;
     private static JSONObject BUSINESS;
+    private static JSONObject DELIVER;
 
     static {
         try {
             PRODUCTS = new JSONObject()
                     .put("title", "Mis Productos")
-                    .put("image", "a.jpg")
+                    .put("image", "https://blog.gs1mexico.org/hubfs/Newsletter/2017/Septiembre%202017/Clasificacion%20de%20Productos%20SAT%20ONU%20UNSPSC%20GS1%20Mexico%20Syncfonia%20Factura%20Blog.png")
                     .put("activity", "a");
             ORDERS = new JSONObject()
-                    .put("title", "Pedidos");
+                    .put("title", "Pedidos")
+                    .put("image", "https://www.consumiblestpv.com/WebRoot/acens/Shops/consumiblestpv_com/547E/E231/7BE2/5F0B/8AC4/4DF0/7515/6434/carro-compra-supermercado.jpg")
+                    .put("activity", "b");
             REPORTS = new JSONObject()
-                    .put("title", "Reportes");
+                    .put("title", "Reportes")
+                    .put("image", "https://i.pinimg.com/originals/6c/d8/b7/6cd8b76dbb55d0108d23d7727df097e5.png")
+                    .put("activity", "c");
             BUSINESS = new JSONObject()
-                    .put("title", "Negocios");
+                    .put("title", "Negocios")
+                    .put("image", "https://i0.wp.com/feriainclusivaempresassb.com/images/feriasb/home.png")
+                    .put("activity", "d");
+            DELIVER = new JSONObject()
+                    .put("title", "Repartidores")
+                    .put("image", "https://image.freepik.com/vector-gratis/servicio-entrega-concepto-mascara_23-2148505104.jpg")
+                    .put("activity", "e");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -33,9 +44,9 @@ public class MenuOptions {
             case "Bussines":
                 return result.put(PRODUCTS).put(ORDERS).put(REPORTS);
             case "Users":
-                return result.put(BUSINESS);
+                return result.put(BUSINESS).put(PRODUCTS).put(DELIVER);
             case "Delivery":
-                    return result;
+                return result.put(ORDERS);
         }
         return result;
     }
